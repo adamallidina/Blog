af9d066133b91a621e443b924c07a4709cef7f97
Adam Allidina <adam.allidina@gmail.com>
2012-08-20 17:50:31 -0600

    fuck git

--- sync.sh
+++ sync.sh
@@ -1,3 +1,2 @@
 jekyll
-rsync -avz --delete  ~/blog/_site/  addies_adamblog@ssh.phx.nearlyfreespeech.net:/home/public
-
+rsync -avz --delete ~/Workspace/Websites/Blog/_site/ addies_adamblog@ssh.phx.nearlyfreespeech.net:/home/public
